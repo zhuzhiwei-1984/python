72a08510a8e3281573abdd8801647d7c35daf516
朱志伟 <zhuzhiwei_1984@outlook.com>
2019-11-23 17:19:29 +0800

    我也不知道更新了什么 哈哈

--- 开始.py
+++ 开始.py
@@ -5,12 +5,24 @@ from flask import Flask, render_template
 
 @全_app.route('/')
 def 视图_主页():
-    return render_template('index.html')
+    return render_template('主页.html')
 
+@全_app.route('/关于我们')
+def 视图_关于我们():
+    return render_template('关于我们.html')
 
-@全_app.route('/user/<name>')
-def 视图_相册(name):
-    return render_template('user.html', name=name)
+@全_app.route('/家庭相册')
+def 视图_家庭相册():
+    return render_template('家庭相册.html')
+
+@全_app.route('/家庭影院')
+def 视图_家庭影院():
+    return render_template('家庭影院.html')
+
+@全_app.route('/家庭书屋')
+def 视图_家庭书屋():
+    return render_template('家庭书屋.html')
+
 
 @全_app.errorhandler(404)
 def 视图_404(e):
